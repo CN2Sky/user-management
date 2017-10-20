2ee55f61a6bae5cd554a3478c1f34cf33df98676
Andrii Fedorenko <a1349252@unet.univie.ac.at>
2017-10-20 16:29:07 +0200

    extended api

--- models/User.js
+++ models/User.js
@@ -3,7 +3,10 @@ var Schema = mongoose.Schema;
 
 var User = new Schema({
     name : { type: String, unique: true },
-    password : String
+    password : String,
+    email : String,
+    type: String,
+    active: Boolean
 });
 
 module.exports = mongoose.model('User', User);
--- user-service-server.js
+++ user-service-server.js
@@ -1,22 +1,22 @@
 'use strict'
 
-var express  = require('express');
-var app      = express();
+var express = require('express');
+var app = express();
 var mongoose = require('mongoose');
-var morgan   = require('morgan');
+var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var config = require('./config/database');
 var jwt = require('jsonwebtoken');
 var port = process.env.PORT || 9091;
 
-var User  = require('./models/User');
+var User = require('./models/User');
 
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({'extended':'true'}));
+app.use(bodyParser.urlencoded({'extended': 'true'}));
 app.use(bodyParser.json());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.json({type: 'application/vnd.api+json'}));
 app.use(methodOverride());
 
 app.use(function (req, res, next) {
@@ -40,50 +40,93 @@ app.listen(port);
 console.log("User Management Service is listening on: " + port);
 
 
-app.post('/users', function(req, res) {
-
-    var user = new User({
-        name: req.body.name,
-        password: req.body.password,
-    });
-
-    user.save(function(err) {
-        if (err) {
-            res.json({ success: false });
-        }
-        else{
-            res.json({ success: true });
-        }
-
-    });
-});
-
-app.get('/users', function(req, res) {
-    User.find({}, function(err, users) {
+app.get('/users', function (req, res) {
+    User.find({}, function (err, users) {
         res.json(users);
     });
 });
 
-app.post('/login', function(req, res) {
+app.get('/user/:username', function (req, res) {
+    User.findOne({name: req.params.username}, function (err, user) {
+        if (err) {
+            return res.json({success: false});
+        } else {
+            return res.json({success: true, user: user});
+        }
+    });
+});
+
+
+app.post('/user/login', function (req, res) {
 
     User.findOne({name: req.body.name, password: req.body.password}, function (err, user) {
         if (err || !user) {
-            return res.json({ success: false, message: 'Authentication failed. User not found.' });
+            return res.json({success: false, message: 'Authentication failed. User not found.'});
         }
 
-        else if(user){
+        else if (user) {
             var token = jwt.sign(user, app.get('superSecret'), {
-                expiresIn : '24h' // expires in 24 hours
+                expiresIn: '24h' // expires in 24 hours
             });
 
-            return res.json({success: true,
+            return res.json({
+                success: true,
                 message: 'Enjoy your token!',
-                token: token});
+                token: token
+            });
         }
         //var decoded = jwt.verify(token, 'testsecret');
         //console.log(decoded._doc.name) // bar
 
-
-
     });
+});
+
+app.post('/user/signup', function (req, res) {
+
+    User.findOne({name: req.body.name}, function (err, userInDb) {
+        if (err || userInDb) {
+            return res.json({success: false, message: 'Username ' + req.body.name + ' already exist'});
+        } else {
+            var user = new User({
+                name: req.body.name,
+                password: req.body.password,
+                email: req.body.email,
+                type: req.body.type ? req.body.type : "user",
+                active: true
+            });
+            user.save(function (err) {
+                if (err) {
+                    return res.json({
+                        success: false,
+                        user: user,
+                        message: err
+                    });
+                }
+                else {
+                    var token = jwt.sign(user, app.get('superSecret'), {
+                        expiresIn: '24h' // expires in 24 hours
+                    });
+
+                    return res.json({
+                        success: true,
+                        user: user,
+                        message: "success",
+                        token : token
+                    });
+                }
+            });
+        }
+    })
+});
+
+app.delete('/user/delete/:username', function (req, res) {
+
+    User.remove({name: req.params.username}, function (err) {
+        if (err) {
+            return res.json({success: false});
+        } else {
+            return res.json({success: true});
+        }
+    });
+
 });
