c337f10b6bff8dea8240cb035cb6fd772826ff24
Andrii Fedorenko <a1349252@unet.univie.ac.at>
2018-02-06 22:02:35 +0100

    manag

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,7 @@ FROM node:7
 WORKDIR /app
 COPY package.json .
 RUN npm install
+RUN npm install -g forever
 COPY . /app
-CMD node server.js
+CMD forever -c 'node --harmony'  server.js
 EXPOSE 8080
--- config/database.js
+++ config/database.js
@@ -1,8 +1,10 @@
+// const db_endpoint = "mongodb://192.168.0.79:27017/n2sky";
+const db_endpoint = "mongodb://192.168.0.102:27017/n2sky";
 
 module.exports = {
     
     'secret': 'testsecret',
-    'database': 'mongodb://192.168.0.79:27017/n2sky',
+    'database': db_endpoint,
     'options': {
         useMongoClient: true,
         db: {native_parser: true},
