748df5209e31ee9a14be0f8c185839d3dd90c92d
Andrii Fedorenko <a1349252@unet.univie.ac.at>
2018-03-08 11:31:11 +0100

    comfig

--- config/database.js
+++ config/database.js
@@ -1,10 +1,10 @@
 // const db_endpoint = "mongodb://192.168.0.79:27017/n2sky";
-const db_endpoint = "mongodb://192.168.0.102:27017/n2sky";
+const config = require('./../HOST.json');
 
 module.exports = {
-    
+
     'secret': 'testsecret',
-    'database': db_endpoint,
+    'database': config.db_host,
     'options': {
         useMongoClient: true,
         db: {native_parser: true},
